51742320650d98d8f77ff72ff8477d407f035088
Richie <rserna2010@gmail.com>
2013-12-02 14:12:15 -0800

    add apt, composer, git cookbooks

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,7 @@ description      'Installs/Configures balanced-php-vm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends "apt"
 depends "php"
+depends "composer"
+depends "git"
--- recipes/client_library.rb
+++ recipes/client_library.rb
@@ -1,2 +1,25 @@
+include_recipe "apt"
 include_recipe "php"
+include_recipe "composer"
+include_recipe "git"
+
+package "php5-curl" do
+  action :install
+end
+
+
+git "#{Chef::Config[:file_cache_path]}/balanced-php" do
+   repository "https://github.com/balanced/balanced-php.git"
+
+   action :sync
+ end
+
+# bash "install_composer" do
+#    cwd "#{Chef::Config[:file_cache_path]}/balanced-php"
+#    user "root"
+#    code <<-EOH
+#      php composer.phar update
+#      EOH
+#    environment 'PREFIX' => "/usr/local"
+# end
 
