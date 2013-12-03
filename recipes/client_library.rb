b9faac7c4c75a1d6f6588c5218c462f839723010
Richie <rserna2010@gmail.com>
2013-12-02 18:02:33 -0800

    add PHPunit, composer calls, and tests

--- recipes/client_library.rb
+++ recipes/client_library.rb
@@ -2,24 +2,53 @@ include_recipe "apt"
 include_recipe "php"
 include_recipe "composer"
 include_recipe "git"
+include_recipe "phpunit"
 
 package "php5-curl" do
   action :install
 end
 
 
+
 git "#{Chef::Config[:file_cache_path]}/balanced-php" do
    repository "https://github.com/balanced/balanced-php.git"
-
+   reference "master"
    action :sync
  end
 
-# bash "install_composer" do
-#    cwd "#{Chef::Config[:file_cache_path]}/balanced-php"
-#    user "root"
-#    code <<-EOH
-#      php composer.phar update
-#      EOH
-#    environment 'PREFIX' => "/usr/local"
-# end
 
+
+
+
+bash "add_composer_files" do
+   cwd "#{Chef::Config[:file_cache_path]}/balanced-php"
+   user "root"
+   code <<-EOH
+    curl -s https://getcomposer.org/installer | php -d detect_unicode=Off
+    php composer.phar update
+     EOH
+   environment 'PREFIX' => "/usr/local"
+end
+
+
+
+bash "install_httpful_and_restful" do
+   cwd "#{Chef::Config[:file_cache_path]}/balanced-php"
+   user "root"
+   code <<-EOH
+     curl -s -L -o httpful.phar https://github.com/downloads/nategood/httpful/httpful.phar
+     curl -s -L -o restful.phar https://github.com/bninja/restful/downloads/restful-{VERSION}.phar
+
+     EOH
+   environment 'PREFIX' => "/usr/local"
+end
+
+
+bash "run_tests" do
+   cwd "#{Chef::Config[:file_cache_path]}/balanced-php"
+   user "root"
+   code <<-EOH
+     phpunit --bootstrap vendor/autoload.php tests/
+     EOH
+   environment 'PREFIX' => "/usr/local"
+end
